b1fae637ff7e79c12f2c514efa94e5113db08301
DopLot <vld.sh2009@gmail.com>
2026-03-25 16:40:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+PORT = 443
+
+USERS = {
+    "my_user": "00000000000000000000000000000001"
+}
+
+# Режим TLS (максимальная конфиденциальность)
+TLS_DOMAIN = "www.google.com"
+
+MODES = {
+    "tls": True,
+    "classic": False,
+    "secure": False
+}
